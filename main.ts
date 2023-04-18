60a21cfcc4a676197367152a3c94a0b0f703b2bc
ExcitedxTurtle <124044194+ExcitedxTurtle@users.noreply.github.com>
2023-04-18 09:40:13 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,12 +10,23 @@ function hardLeft () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 40)
     basic.pause(100)
 }
+function FullBack () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 40)
+    basic.pause(100)
+}
 function avoid () {
     fullStop()
+    FullBack()
     // Turn right and move forward to go around the obstacle
     softRight()
     basic.pause(500)
-    // Move around the obstacle
     while (maqueen.Ultrasonic(PingUnit.Centimeters) < 30) {
         if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
             // If too close to the obstacle, turn right and move forward
@@ -89,22 +100,21 @@ basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
         avoiding = 1
         avoid()
-        LFSL = 0
     } else {
         avoiding = 0
         LFSR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
         LFSL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
-        if (LFSL == 1 && LFSR == 1) {
-            fullForwards()
-        } else if (LFSL == 1) {
-            softRight()
-        } else if (LFSR == 1) {
-            softLeft()
-        } else {
-            fullStop()
-            basic.pause(100)
-            hardLeft()
-            basic.pause(100)
-        }
+    }
+    if (LFSL == 1 && LFSR == 1) {
+        fullForwards()
+    } else if (LFSL == 1) {
+        softRight()
+    } else if (LFSR == 1) {
+        softLeft()
+    } else {
+        fullStop()
+        basic.pause(100)
+        hardLeft()
+        basic.pause(100)
     }
 })
